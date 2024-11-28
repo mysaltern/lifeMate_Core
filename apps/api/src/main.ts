4195d216f4d2f8cc2cdf8f4ca362d2f31fcd163c
mohammad <mysaltern@gmail.com>
2024-11-28 11:42:46 +1100

    enable ValidationPipe

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { ApiGatewayModule } from './app.module';
-
+import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(ApiGatewayModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AuthModule } from './auth.module';
-
+import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);  
-
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3002);
 }
 bootstrap();   
